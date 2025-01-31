ce7ec100c57d2fee5380ee12fcc0f7b76129b8d4
Richard Langner <31290551+RichardLangner@users.noreply.github.com>
2025-01-31 07:34:15 +0000

    Cleaned up the code a bit

--- src/main.cpp
+++ src/main.cpp
@@ -33,22 +33,6 @@ void setup() {
 
     // Announce that the RFID scanning works
     Serial.println("Place your RFID tag near the reader...");
-
-    // Clear the stored file contents
-    clearFile("/rfidData.txt");
-    writeToFile("00E6021F982846");
-   // Print the contents of the stored file for comparison
-    Serial.println("Contents of the stored file:");
-    File file = LittleFS.open("/rfidData.txt", "r");
-    if (file) {
-        while (file.available()) {
-            Serial.write(file.read());
-        }
-        file.close();
-    } else {
-        Serial.println("Failed to open the file for reading.");
-    }
-    Serial.println();
 }
 
 void loop() {
@@ -70,28 +54,15 @@ void loop() {
         rfidData=rfidData.substring(1, rfidData.length()-2);
         Serial.print("Raw RFID Data: ");
         Serial.println(rfidData);
+        const char* filename =("/" + rfidData).c_str();
+         if (LittleFS.exists(filename)) {
+        Serial.println("File exists!");
+    } else {
+        Serial.println("File does not exist.");
+    }
         rfidData="";
         }
     }
-
-    //         // Compare the raw data directly with the known tag UID
-    //         // String knownTag = rfidData;  // Use current RFID data as knownTag
-
-    //         // if (rfidData.equals(knownTag)) {
-    //             // Serial.println("Tag match found - Turning on LED");
-    //             // digitalWrite(BUILTIN_LED, LOW);  // Turn on the built-in LED (LOW for active low LED)
-    //         // } else {
-    //             // Serial.println("No match found");
-    //             // digitalWrite(BUILTIN_LED, HIGH);  // Turn off the built-in LED
-    //         // }
-
-    //         // Write data to a file
-    //         // writeToFile(rfidData.c_str());
-    //     } else {
-    //         Serial.println("No data available from RFID reader.");
-    //         digitalWrite(BUILTIN_LED, HIGH);  // Ensure the LED stays off when no data is available
-    //     }
-    // }
 }
 
 // Function to clear the contents of a file
